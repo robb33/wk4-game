c667757457deab7f0d6eba5d4b275cedfb204137
robb33 <rbenny@earthlink.net>
2016-12-01 18:30:58 -0500

    first commit

---
+++ javascript/game.js
@@ -0,0 +1,12 @@
+//Sanity test
+
+document.getElementById("test").innerHTML = "Crystal Collector Game.....!";
+
+
+document.getElementById("win").innerHTML = "Wins";
+
+
+document.getElementById("loss").innerHTML = "Losses";
+
+
+document.getElementById("score").innerHTML = "Your Total Score is:";
