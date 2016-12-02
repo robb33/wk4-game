610aa5c9e7ee2b8817cb489511423545baf98ad3
robb33 <rbenny@earthlink.net>
2016-12-02 16:23:52 -0500

    buttons and some testing

--- javascript/game.js
+++ javascript/game.js
@@ -1,12 +1,74 @@
 //Sanity test
 
+
+
+
+var win = 0;
+var loss = 0;
+
+
 document.getElementById("test").innerHTML = "Crystal Collector Game.....!";
 
 
 document.getElementById("win").innerHTML = "Wins";
 
+// function win(){
+//           counter++;
+//           $('#win').text(counter);
+//         }
+
 
 document.getElementById("loss").innerHTML = "Losses";
 
+// function loss(){
+//           counter++;
+//           $('#loss').text(counter);
+//         }
+
 
 document.getElementById("score").innerHTML = "Your Total Score is:";
+
+// function score(){
+//           counter++;
+//           $('#score').text(counter);
+//         }
+
+
+
+
+// test button
+// function myFunction() {
+//     // document.getElementById("demo").innerHTML = "Hello World";
+//     alert("hello world");
+// }
+
+	// Arrakis gem
+function arra() {
+    
+    alert("Arrakis");
+}
+
+
+	// Caladan gem
+function cala() {
+    
+    alert("Caladan");
+}
+
+
+	// Tupile gem
+function tupi() {
+    
+    alert("Tupile");
+}
+
+
+	// Ix gem
+function ix() {
+    
+    alert("Ix");
+}
+
+
+
+// ....just M:€....
